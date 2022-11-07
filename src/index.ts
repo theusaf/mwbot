f830935893f0fac03a5ca49bc33b798acfc9f29d
theusaf <theusafyt@gmail.com>
2022-11-07 15:19:53 -0800

    feat: implement login, logs, requests

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,9 @@ import {
   BotState,
   MWVersion,
   RequestOptions,
+  GotRequest,
+  MWLoginResponse,
+  MWLoginForm,
 } from "./types.js";
 import { CookieJar } from "tough-cookie";
 import { dirname, join } from "node:path";
@@ -24,9 +27,7 @@ export default class MWBot {
   createAccountToken: string;
   mwVersion: MWVersion;
   counter: Counter;
-  customOptions: BotOptions;
   options: BotOptions;
-  customRequestOptions: RequestOptions;
   requestOptions: RequestOptions;
 
   constructor(options: BotOptions, requestOptions: RequestOptions) {
@@ -41,35 +42,137 @@ export default class MWBot {
       fulfilled: 0,
       rejected: 0,
     };
-    this.customOptions = options ?? {};
-    this.options = MWBot.merge(
-      {
-        verbose: false,
-        silent: false,
-        defaultSummary: "MWBot",
-        concurrency: 1,
-        apiUrl: false,
-        sparqlEndpoint:
-          "https://query.wikidata.org/bigdata/namespace/wdq/sparql", // Wikidata
+    this.options = {
+      verbose: false,
+      defaultSummary: "MWBot",
+      concurrency: 1,
+      apiUrl: null,
+      sparqlEndpoint: "https://query.wikidata.org/bigdata/namespace/wdq/sparql", // Wikidata
+    };
+    this.setOptions(options);
+
+    this.requestOptions = {
+      method: "POST",
+      headers: {
+        "User-Agent": `mwbot/${packageJson.version}`,
       },
-      this.customOptions
-    );
-    this.customRequestOptions = requestOptions ?? {};
-    this.requestOptions = MWBot.merge<RequestOptions>(
-      {
-        method: "POST",
-        headers: {
-          "User-Agent": `mwbot/${packageJson.version}`,
-        },
-        responseType: "json",
-        form: {},
-        timeout: {
-          request: 120000,
-        },
-        cookieJar: new CookieJar(),
+      resolveBodyOnly: false,
+      form: {},
+      timeout: {
+        request: 120000,
       },
-      this.customRequestOptions
-    );
+      cookieJar: new CookieJar(),
+    };
+    this.setRequestOptions(requestOptions);
+  }
+
+  /**
+   * Sets and overwrites mwbot options.
+   *
+   * @param options
+   */
+  setOptions(options: BotOptions): void {
+    this.options = MWBot.merge(this.options, options);
+  }
+
+  /**
+   * Sets and overwrites request options.
+   *
+   * @param requestOptions
+   */
+  setRequestOptions(requestOptions: RequestOptions): void {
+    delete requestOptions.resolveBodyOnly;
+    this.requestOptions = MWBot.merge(this.requestOptions, requestOptions);
+  }
+
+  setApiUrl(apiUrl: string): void {
+    this.options.apiUrl = apiUrl;
+  }
+
+  request<E>(params: Object, customRequestOptions: RequestOptions = {}) {
+    return this.requestJSON<E>(params, customRequestOptions);
+  }
+
+  requestJSON<E>(params: Object, customRequestOptions: RequestOptions = {}) {
+    const requestOptions = this.prepareRequest(params, customRequestOptions);
+    return this.rawRequestJSON<E>(requestOptions);
+  }
+
+  requestText(params: Object, customRequestOptions: RequestOptions = {}) {
+    const requestOptions = this.prepareRequest(params, customRequestOptions);
+    return this.rawRequestText(requestOptions);
+  }
+
+  prepareRequest(params: Object, customRequestOptions: RequestOptions) {
+    if (!this.requestOptions.url) this.requestOptions.url = this.options.apiUrl;
+    let requestOptions = MWBot.merge(this.requestOptions, customRequestOptions);
+    requestOptions.form = MWBot.merge(requestOptions.form, params);
+    return requestOptions;
+  }
+
+  async login(options: BotOptions): Promise<void> {
+    this.options = MWBot.merge(this.options, options);
+    const { username, password, apiUrl } = this.options;
+    if (!username || !password || !apiUrl) {
+      throw new Error("Missing login credentials.");
+    }
+    const loginForm: MWLoginForm = {
+      action: "login",
+      lgname: username,
+      lgpassword: password,
+    };
+
+    const loginResponse = await this.request<MWLoginResponse>(loginForm);
+    if (!loginResponse.login?.result) {
+      this.log(
+        `Login failed with invalid response: ${username}@${apiUrl
+          .split("/api.php")
+          .join("")}`
+      );
+      throw new Error("Invalid response from API.");
+    } else {
+      this.state = MWBot.merge(this.state, loginResponse.login);
+      loginForm.lgtoken = loginResponse.login.token;
+      return this.request(loginForm);
+    }
+  }
+
+  async rawRequestJSON<E = Object>(requestOptions: RequestOptions): Promise<E> {
+    this.counter.total++;
+    this.counter.resolved++;
+    try {
+      const body = await this.rawRequest(requestOptions).json();
+      this.counter.fulfilled++;
+      return body as E;
+    } catch (error) {
+      this.counter.rejected++;
+      throw error;
+    }
+  }
+
+  async rawRequestText(requestOptions: RequestOptions): Promise<string> {
+    this.counter.total++;
+    this.counter.resolved++;
+    try {
+      const body = await this.rawRequest(requestOptions).text();
+      this.counter.fulfilled++;
+      return body as string;
+    } catch (error) {
+      this.counter.rejected++;
+      throw error;
+    }
+  }
+
+  rawRequest(requestOptions: RequestOptions): GotRequest {
+    return got(requestOptions) as GotRequest;
+  }
+
+  log(log: string) {
+    if (this.options.verbose) console.log(`[mwbot] ${log}`);
+  }
+
+  get version(): string {
+    return packageJson.version;
   }
 
   static merge<E>(a: E, b: E): E {
--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,32 @@
-import { OptionsInit } from "got";
+import { CancelableRequest, OptionsInit, Request } from "got";
 
-export interface BotOptions {}
+export interface BotOptions {
+  apiUrl?: string;
+  verbose?: boolean;
+  defaultSummary?: string;
+  concurrency?: number;
+  sparqlEndpoint?: string;
+  username?: string;
+  password?: string;
+}
 export interface BotState {}
 export interface MWVersion {}
+export interface MWForm {
+  action: string;
+  [x: string]: any;
+}
+export interface MWLoginForm extends MWForm {
+  action: string;
+  lgname: string;
+  lgpassword: string;
+  lgtoken?: string;
+}
+export interface MWLoginResponse {
+  login?: {
+    result?: string;
+    token?: string;
+  };
+}
 export interface Counter {
   total: number;
   resolved: number;
@@ -10,3 +34,4 @@ export interface Counter {
   rejected: number;
 }
 export type RequestOptions = OptionsInit;
+export type GotRequest = CancelableRequest<Request>;
